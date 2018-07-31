e648e12018ea7cdd196edbd6fbb82e3d3341f6a0
me <famlinkus@gmail.com>
2018-07-31 20:24:13 +0530

    update

--- app.py
+++ app.py
@@ -43,6 +43,18 @@ def redirect_no_referer():
     return html_minify(render_template("redirect.html", url=url))
 
 
+@app.before_request
+def enforce_https():
+    if (
+        request.endpoint in app.view_functions
+        and not request.is_secure
+        and not "127.0.0.1" in request.url
+        and not "localhost" in request.url
+        and not "192.168." in request.url
+    ):
+        return redirect(request.url.replace("http://", "https://"), code=301)
+
+
 @app.route("/search/get_results/", methods=["GET"])
 def scrape_results():
     query = request.args.get("q")
